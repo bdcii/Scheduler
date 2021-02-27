66ca1640727f74b7c919aa4657a9cd34cfc44e99
bdcii <darrell.chess@gmail.com>
2021-02-27 18:23:27 -0500

    added js code to test local storage access functionality

--- script.js
+++ script.js
@@ -1,5 +1,7 @@
-let displayEl = $('#currentDay')
+let displayEl = $('#currentDay');
 var input = $("<input />");
+
+var taskEl = document.querySelector("#task");
 
 
 
@@ -8,6 +10,22 @@ setInterval(function(){
     displayEl.text(currentTime.format('MMMM Do YYYY, h:mm:ss a'));
 }, 1000)
 
+
+
+renderSavedTask();
+
+function renderSavedTask() {
+    var task = localStorage.getItem("task");
+ 
+  
+    if (!task) {
+      return;
+    }
+
+
+  
+    taskEl.textContent = task;
+  }
 
 var container = $('.container')
 var table = $('<table>')
@@ -25,7 +43,21 @@ for (i = 9; i < 18; i++){
     table.append(tr)
 
     $(tr).addClass("row time-block" );
-    $(tdHour).addClass('hour col-lg-2');
-     $(tdTask).addClass('col-lg-8 description textarea').append("<textarea></textarea>");
-    $(tdSave).addClass('saveBtn col-lg-2');
+    $(tdHour).addClass('hour col-xl-2');
+     $(tdTask).addClass('col-xl-8 description textarea').append("<textarea id='#task'></textarea>")
+    $(tdSave).addClass('saveBtn fa fa-save col-xl-2 saveBtn i:hover').append("<button id='#save'></button")
 }
+
+var saveButton = document.querySelector("#save");
+
+saveButton.addEventListener('click', function(event){
+    event.preventDefault();
+
+    var task = document.querySelector("#task").value;
+
+    localStorage.setItem("task", task);
+
+
+});
+
+
